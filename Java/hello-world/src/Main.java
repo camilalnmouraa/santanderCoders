790e3fdb0b6cc62ad7061a9b268fef616b565fed
Camila Moura <camilalnmoura@gmail.com>
2023-05-30 11:44:54 -0300

    Estruturas Condicionais

--- Java/hello-world/src/Main.java
+++ Java/hello-world/src/Main.java
@@ -4,29 +4,34 @@ public class Main {
 
     public static void main(String[] args) {
 
-        boolean fimDeSemana = false;
-        boolean fazendoSol = true;
-        boolean vamosAPraia = fimDeSemana && fazendoSol;
+        int nota = 100;
+        String graduacao;
 
-        //tabela verdade
+        // A 80 B 70 C 60 D 0
+        if (nota >= 80) {
+            graduacao = "A";
+        } else if (nota < 80 && nota >=70) {
+            graduacao = "B";
+        } else if (nota < 70 && nota >= 60) {
+            graduacao = "C";
+        } else if (nota < 60 && nota >= 0){
+            graduacao = "D";
+        } else {
+            graduacao = "";
+        }
 
-        //operador && (AND)
-        // true && true = true
-        // true && false = false
-        // false && true = false
-        // false && false = false
-
-        //Operador || (OR)
-        // true || true = true
-        // true || false = true
-        // false || true = true
-        // false || false = false
-
-
-        System.out.println(vamosAPraia);
-
-        String mensagem = fimDeSemana ? "É fim de Semana" : "Não é fim de semana";
-        System.out.println(mensagem);
+        switch (graduacao) {
+            case "A":
+            case "B":
+                System.out.println("Aluno Aprovado!");
+                break;
+            case "C":
+            case "D":
+                System.out.println("Aluno Reprovado!");
+                break;
+            default:
+                System.out.println("Graduação Inválida!");
+        }
 
     }
 }
